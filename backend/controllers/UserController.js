374830f32dc7382c550e64e43a4f4c6aad7343cb
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-09 21:54:16 +0700

    feat(backend): add basic controller

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,10 +2,30 @@ const User = require('../models/User');
 
 const ctrl = {
   //GET /users
-  getUsers: (req, res) => {
-    User.find().then((users) => {
-      return res.send(users);
-    });
+  getUsers: async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (e) {
+      return res.status(400).json({
+        error: 'No users found',
+      });
+    }
+  },
+
+  //POST /users/create
+  createUser: async (req, res) => {
+    const user = new User(req.body);
+    try {
+      const newUser = await user.save();
+      console.log(newUser);
+      res.json({ newUser });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        error: 'Cant create new user',
+      });
+    }
   },
 };
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const routes = express.Router();
+
 const ctrl = require('../controllers/UserController');
 // const {} = require('../middlewares/AuthMiddleware');
 
 routes.route('/').get(ctrl.getUsers);
+
+routes.route('/create').post(ctrl.createUser);
 
 module.exports = routes;
