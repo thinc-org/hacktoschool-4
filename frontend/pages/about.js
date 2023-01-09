880dca07ee0253429aa6c560fac8658ec3d8bde1
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-09 15:01:52 +0700

    Initial commit from Create Next App

---
+++ frontend/pages/about.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+
+      <Link href="/">Back home</Link>
+    </div>
+  );
+};
+
+export default AboutPage;
