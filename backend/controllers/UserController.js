57d0e3b4cac7f8d267a2f77fb26b2a9570945a22
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-10 19:49:32 +0700

    feat(backend): register route

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
-const User = require('../models/User');
+const { User, validate } = require('../models/User');
+const bcrypt = require('bcrypt');
 
 const ctrl = {
   //GET /users
@@ -25,6 +26,28 @@ const ctrl = {
       return res.status(400).json({
         error: 'Cant create new user',
       });
+    }
+  },
+
+  //POST /users/register
+  registerUser: async (req, res) => {
+    try {
+      const { error } = validate(req.body);
+
+      // Some Validate Error
+      if (error) {
+        return res.status(400).send({ message: error.details[0].message });
+      }
+      const user = await User.findOne({ name: req.body.name });
+      if (user) {
+        return res.status(409).send({ message: 'Username is already in used' });
+      }
+      const salt = await bcrypt.genSalt(Number(process.env.SALT));
+      const hashPassword = await bcrypt.hash(req.body.password, salt);
+      await new User({ ...req.body, password: hashPassword }).save();
+      return res.status(201).send({ message: 'User created successfully' });
+    } catch (e) {
+      return res.status(500).send({ message: 'Internal Server Error' });
     }
   },
 };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const routes = express.Router();
-
 const ctrl = require('../controllers/UserController');
-// const {} = require('../middlewares/AuthMiddleware');
 
 routes.route('/').get(ctrl.getUsers);
 
 routes.route('/create').post(ctrl.createUser);
+
+routes.route('/register').post(ctrl.loginUser);
 
 module.exports = routes;
