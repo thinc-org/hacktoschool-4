5a53d0cb3ede2ac2e96ad007354beef5eff3fd0d
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-10 22:21:46 +0700

    feat(backend): auth middleware

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 const ctrl = require('../controllers/UserController');
+const { authenticateToken } = require('../middlewares/AuthMiddleWare');
 
 routes.route('/').get(ctrl.getUsers);
 
 routes.route('/create').post(ctrl.createUser);
 
 routes.route('/register').post(ctrl.registerUser);
+
+routes.route('/testAuthenToken').get(authenticateToken, ctrl.getUsers);
 
 module.exports = routes;
