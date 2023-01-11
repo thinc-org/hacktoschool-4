cc1bc1e48fa6d5e853d20d49cea9f3097fffb25a
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-11 22:21:27 +0700

    refactor(backend): change to async await function

--- backend/controllers/CourseController.js
+++ backend/controllers/CourseController.js
@@ -2,10 +2,15 @@ const Course = require('../models/Course');
 
 const ctrl = {
   //GET /courses
-  getCourses: (req, res) => {
-    Course.find().then((courses) => {
-      return res.status(200).send(courses);
-    });
+  getCourses: async (req, res) => {
+    try {
+      const courses = await Course.find();
+      return res.status(200).json(courses);
+    } catch (e) {
+      return res.status(400).json({
+        error: 'No courses found',
+      });
+    }
   },
 };
 
