29f2df7dbf3338d56aa0b48581cb78b190fc522c
ffahf <fahfi.niti@gmail.com>
2023-01-11 21:09:05 +0700

    add CourseCard
    add CourseCard and course sample

--- frontend/src/components/CourseCard.js
+++ frontend/src/components/CourseCard.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const CourseCard = () => {
+const CourseCard = ({ course }) => {
+    console.log(course);
     return (
-        <>
+        //if that course is in that student list there will be no button
+        <div className="course">
             <div>
-                <h2>course</h2>
-                <button>enroll</button>
-            </div>
-        </>
-    );
-};
+                <h2>{course.title}</h2>
+                <h3>{course.description}</h3>
+                <h3>{course.instructor}</h3>
+                <button>enrolled</button>
 
+            </div>
+        </div>
+    );
+}
 export default CourseCard;
--- frontend/src/pages/CoursesPage.js
+++ frontend/src/pages/CoursesPage.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import CourseCard from '../components/CourseCard';
 
+const course1 = {
+
+    "description": "fah",
+    "instructor": " build",
+    "students": ["pun", "book"],
+    "announcement": "pain in the ass",
+    "createdAt": new Date(),
+
+}
+
+const course2 = {
+
+    "description": "fahfi",
+    "instructor": " build",
+    "students": ["tuatua", "book"],
+    "announcement": "ifififififfi",
+    "createdAt": new Date(),
+
+}
+const course3 = {
+
+    "description": "niniini",
+    "instructor": " builddddd",
+    "students": ["tuatua", "book"],
+    "announcement": "ifififififfi",
+    "createdAt": new Date(),
+
+}
+const courses = [course1, course2, course3];
+
 const CoursesPage = () => {
     return (
         <>
-            <div>
-                <CourseCard></CourseCard>
-                <CourseCard></CourseCard>
+            <div className="enrolled-courses-wrapper">
+                <div className="enrolled-courses-header">
+                    <h1>enrolled</h1>
+                </div>
+                <div className="enrolled-courses">
+                    <div className="course" style={{ width: "257.5px", marginRight: "30px" }}>
+                        {courses.map((course) => (
+                            <CourseCard course={course} />
+                        ))}
+                    </div>
+                </div>
             </div>
         </>
     );
