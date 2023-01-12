fc62fb99b1e670fa2a36c165143bbd2f79506ea8
ponnabook <88271077+ponnabook@users.noreply.github.com>
2023-01-12 15:14:25 +0700

    feat(frontend): create Font.js

---
+++ frontend/src/components/Font.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+  & h1 {
+    font-family: 'Dela Gothic One', cursive;
+    font-size: 56px;
+    line-height: 120%;
+  }
+
+  & h2 {
+    font-family: 'Dela Gothic One', cursive;
+    font-size: 48px;
+    line-height: 100%;
+  }
+
+  & h3 {
+    font-family: 'Dela Gothic One', cursive;
+    font-size: 32px;
+    line-height: 100%;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  & h4 {
+    font-family: 'Dela Gothic One', cursive;
+    font-size: 26px;
+    line-height: 100%;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  & NavLink {
+    font-family: 'Dela Gothic One', cursive;
+    font-size: 26px;
+    line-height: 100%;
+    padding: 0px;
+    margin: 0px;
+  }
+`;
