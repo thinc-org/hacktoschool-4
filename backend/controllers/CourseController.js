2857f629589e53936f15f37957ce18d057ea4574
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-12 10:48:21 +0700

    feat(backend): course Create Read api

--- backend/controllers/CourseController.js
+++ backend/controllers/CourseController.js
@@ -12,6 +12,45 @@ const ctrl = {
       });
     }
   },
+
+  //GET /courses/id/:id
+  getCourseByID: async (req, res) => {
+    try {
+      const course = await Course.findById(req.params.id);
+      return res.status(200).json(course);
+    } catch (e) {
+      return res.status(400).json({
+        error: 'No course found',
+      });
+    }
+  },
+
+  //GET /courses/title/:title
+  getCourseByTitle: async (req, res) => {
+    try {
+      const course = await Course.findOne({ title: req.params.title });
+      return res.status(200).json(course);
+    } catch (e) {
+      return res.status(400).json({
+        error: 'No user found',
+      });
+    }
+  },
+
+  //POST /courses/create
+  createCourse: async (req, res) => {
+    const course = new Course(req.body);
+    try {
+      const newCourse = await course.save();
+      console.log(newCourse);
+      return res.status(201).json({ newCourse });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        error: 'Cant create new course',
+      });
+    }
+  },
 };
 
 module.exports = ctrl;
--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -19,4 +19,52 @@ const ctrl = require('../controllers/CourseController');
  */
 routes.route('/').get(ctrl.getCourses);
 
+/**
+ * @swagger
+ * /courses/id/{id}:
+ *  get:
+ *    tags:
+ *      - courses
+ *    summary: Find course by ID
+ *    description: Returns a single course
+ *    parameters:
+ *      - name: _id
+ *        in: path
+ *        description: mongodb ID of course to return
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: successful operation
+ *      '400':
+ *        description: Invalid ID supplied
+ */
+routes.route('/id/:id').get(ctrl.getCourseByID);
+
+/**
+ * @swagger
+ * /courses/title/{title}:
+ *  get:
+ *    tags:
+ *      - courses
+ *    summary: Find course by title
+ *    description: Returns a single course
+ *    parameters:
+ *      - name: title
+ *        in: path
+ *        description: title of course to return
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: successful operation
+ *      '400':
+ *        description: Invalid username supplied
+ */
+routes.route('/title/:title').get(ctrl.getCourseByTitle);
+
+routes.route('/create').post(ctrl.createCourse);
+
 module.exports = routes;
