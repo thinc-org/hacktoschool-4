f26f56d631c7a18920a2d661b0c5125a76dc8c4f
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-12 19:36:12 +0700

    feat(frontend): try instructor route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,11 @@ import { COLORS } from './components/Colors';
 import Navbar from './components/NavBar';
 import SignInPage from './pages/SignInPage';
 import Home from './pages';
-import CoursesPage from './pages/CoursesPage';
 import Footer from './components/Footer';
 import SignUpPage from './pages/SignUpPage';
+import InstructorRoute from './routes/InstructorRoutes';
+import CoursesPage from './pages/CoursesPage';
+import CreateCourse from './pages/CreateCourse';
 
 const theme = {
   colors: COLORS,
@@ -22,8 +24,14 @@ function App() {
           <Routes>
             <Route path="/" exact element={<Home />} />
             <Route path="/courses" exact element={<CoursesPage />} />
+            <Route
+              path="/courses"
+              exact
+              element={<InstructorRoute Component={CoursesPage} to="/test" />}
+            />
             <Route path="/sign-in" element={<SignInPage />} />
             <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/create/course" element={<CreateCourse />} />
           </Routes>
           <Footer></Footer>
         </Container>
