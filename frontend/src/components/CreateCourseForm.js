fad40c50ddc58f4db418929ef263105e7c53307f
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-12 13:46:34 +0700

    feat(frontend): CreateCourse page

---
+++ frontend/src/components/CreateCourseForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { registerUser } from '../api/UserAPI';
+
+const CreateCourseForm = () => {
+  const [data, setData] = useState({
+    title: '',
+    description: '',
+  });
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = ({ currentTarget: input }) => {
+    console.log(input.name, input.value);
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // TODO: CREATE COURSE API NOT FINISHED
+      navigate('/dashboard');
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+  return (
+    <>
+      <div>
+        <h2>Create Course</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            onChange={handleChange}
+            value={data.title}
+            required
+          ></input>
+          <label htmlFor="description">description:</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            onChange={handleChange}
+            value={data.description}
+          ></input>
+          {error && <div>{error}</div>}
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default CreateCourseForm;
---
+++ frontend/src/pages/CreateCourse.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateCourseForm from '../components/CreateCourseForm';
+const CreateCourse = () => {
+  return (
+    <>
+      <div>
+        <CreateCourseForm></CreateCourseForm>
+      </div>
+    </>
+  );
+};
+
+export default CreateCourse;
