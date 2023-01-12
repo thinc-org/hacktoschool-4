1ac1406e4615f6676499e3da0db1af3c2c817610
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-12 12:50:32 +0700

    docs(backend): add more swagger

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -67,8 +67,50 @@ routes.route('/title/:title').get(ctrl.getCourseByTitle);
 
 routes.route('/create').post(ctrl.createCourse);
 
+/**
+ * @swagger
+ * /courses/delete/id/{id}:
+ *  delete:
+ *    tags:
+ *      - courses
+ *    summary: Delete course by ID
+ *    description: Delete a single course
+ *    parameters:
+ *      - name: _id
+ *        in: path
+ *        description: mongodb ID of course to delete
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: successful operation
+ *      '400':
+ *        description: Invalid ID supplied
+ */
 routes.route('/delete/id/:id').delete(ctrl.deleteCourseByID);
 
+/**
+ * @swagger
+ * /courses/delete/title/{title}:
+ *  delete:
+ *    tags:
+ *      - courses
+ *    summary: Delete course by title
+ *    description: Delete a single course
+ *    parameters:
+ *      - name: title
+ *        in: path
+ *        description: title of course to delete
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: successful operation
+ *      '400':
+ *        description: Invalid username supplied
+ */
 routes.route('/delete/title/:title').delete(ctrl.deleteCourseByTitle);
 
 module.exports = routes;
