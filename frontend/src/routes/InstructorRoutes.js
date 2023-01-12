09d967e06f846b4ba0ef29e5d93a8fc46641513d
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-12 12:42:17 +0700

    feat(frontend): Instructor Routes

---
+++ frontend/src/routes/InstructorRoutes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { isInstructor } from '../api/AuthAPI';
+
+const InstructorRoute = ({ Component, to }) => {
+  console.log(to);
+  return isInstructor() ? <Component /> : <Navigate to={to} />;
+};
+
+export default InstructorRoute;
