7bffb6d91b5c4c1379fbddac3ddd3ef07b470d5e
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-13 08:13:17 +0700

    fix(frontend): remove unnecessary export

--- frontend/src/api/UserAPI.js
+++ frontend/src/api/UserAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const basedURL = 'http://localhost:8080/api/'; // in dev ==> http://localhost:8080/api/
+const basedURL = 'http://localhost:8080/api/'; // in dev ==> http://localhost:8080/api/
 
 export const registerUser = async (data) => {
   const url = `${basedURL}users/register`;
