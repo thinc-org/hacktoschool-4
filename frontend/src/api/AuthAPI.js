23d7c101f223a104355a64e1288666d520bf9faf
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-01-13 11:45:15 +0700

    fix(frontend): remove username in localStorage
    remove username in localStorage when logout

--- frontend/src/api/AuthAPI.js
+++ frontend/src/api/AuthAPI.js
@@ -16,6 +16,7 @@ export const loginUser = async (data) => {
 export const logoutUser = async (data) => {
   localStorage.removeItem('jwt');
   localStorage.removeItem('role');
+  localStorage.removeItem('username');
 };
 
 export const isAutheticated = () => {
