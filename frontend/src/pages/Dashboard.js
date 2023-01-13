c8a01ceb0ae2ac43fd9066b693293c8d184dcc9c
ffahf <fahfi.niti@gmail.com>
2023-01-13 21:23:47 +0700

    fix(frontend):fix submit style, dash

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,32 +2,35 @@ import React, { useEffect, useState } from 'react';
 import CardStyle from '../components/CourseCard.module.css';
 import CourseCard from '../components/CourseCard';
 import { getCourseByUsername } from '../api/CourseAPI';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   console.log('get');
   const [coursesByInst, setCoursesByInst] = useState([]);
 
   console.log("pain in the ass");
-  useEffect(() => {
-    console.log("pain in the ass2");
-    getCourseByUsername(localStorage.getItem("username")).then((courses) => setCoursesByInst(courses));
-    console.log('get courses from instr done');
-    console.log(coursesByInst);
-  }, []);
+  // useEffect(() => {
+  //   console.log("pain in the ass2");
+  //   getCourseByUsername(localStorage.getItem("username")).then((courses) => setCoursesByInst(courses));
+  //   console.log('get courses from instr done');
+  //   console.log(coursesByInst);
+  // }, []);
 
   return (<><div style={{ margin: '30px', width: '100%' }}>
     <h1 className="courses-header"> DASHBOARD PAGE</h1>
     <div className={CardStyle.wrapper}>
       <div>
-        {
+        {/* {
           coursesByInst.map((course) => (
             <CourseCard course={course} isEnrolled={true} />
           ))
             (coursesByInst.length === 0) ? (<body>no course found</body>) : (<body></body>)
-        }
+        } */}
 
       </div>
-
+      {
+        <Link to="../courses/create" className={CardStyle.course.a}>create</Link>
+      }
     </div>
   </div>
 
